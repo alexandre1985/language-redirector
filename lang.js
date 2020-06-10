b91322f52311e0a19407af9aec3c76f71d54a6b1
Daniel Santos <hello@brighterdan.com>
2020-06-10 12:53:14 +0100

    ignoring bots

--- lang.js
+++ lang.js
@@ -1,64 +1,69 @@
-// do you want to make distinctions of countries with the same language?
-const doDistinctionsOfCountriesWithTheSameLanguage = false
+(function() {
+    // ignoring bots
+    if(/bot/i.test(navigator.userAgent)) { return }
 
-// put in here all the languages of your website
-// if you DON'T want to make distinctions of countries with the same language put the root language in this variable, such as, en or es or pt (instead of en-US or pt-BR or es-MX)
-const languagesOfWebsite = [ 'pt', 'en' ]
+    // do you want to make distinctions of countries with the same language?
+    const doDistinctionsOfCountriesWithTheSameLanguage = false
 
-// default language
-const languageOfRootWebpage = languagesOfWebsite[0];
+    // put in here all the languages of your website
+    // if you DON'T want to make distinctions of countries with the same language put the root language in this variable, such as, en or es or pt (instead of en-US or pt-BR or es-MX)
+    const languagesOfWebsite = [ 'pt', 'en' ]
 
-const pathnamesOfAllLanguagesOfWebsite= languagesOfWebsite.map(lang => `/${lang}/`)
+    // default language
+    const languageOfRootWebpage = languagesOfWebsite[0];
 
-// browser language
-let browserLang = navigator.language ? navigator.language : navigator.userLanguage;
+    const pathnamesOfAllLanguagesOfWebsite= languagesOfWebsite.map(lang => `/${lang}/`)
 
-if ( (browserLang) && (! doDistinctionsOfCountriesWithTheSameLanguage) ) {
-	const separatorIndex = browserLang.indexOf('-')
-	browserLang = (separatorIndex) ? browserLang.substr(0, separatorIndex) : browserLang
-}
+    // browser language
+    let browserLang = navigator.language ? navigator.language : navigator.userLanguage;
 
-const isBrowserLanguageSameLanguageAsRootWebpage = ( browserLang === languageOfRootWebpage )
+    if ( (browserLang) && (! doDistinctionsOfCountriesWithTheSameLanguage) ) {
+        const separatorIndex = browserLang.indexOf('-')
+        browserLang = (separatorIndex) ? browserLang.substr(0, separatorIndex) : browserLang
+    }
 
-// is language being forced
-let urlParams = new URLSearchParams(location.search);
-const automaticLanguageRedirectionStatus = urlParams.get('languageRedirection')
+    const isBrowserLanguageSameLanguageAsRootWebpage = ( browserLang === languageOfRootWebpage )
 
-const isPathnameTheDefaultLanguage = ( location.pathname === pathnamesOfAllLanguagesOfWebsite[0] )
+    // is language being forced
+    let urlParams = new URLSearchParams(location.search);
+    const automaticLanguageRedirectionStatus = urlParams.get('languageRedirection')
 
-const actualPathname = isPathnameTheDefaultLanguage ? '/' : location.pathname
+    const isPathnameTheDefaultLanguage = ( location.pathname === pathnamesOfAllLanguagesOfWebsite[0] )
 
-const hashOfLocation = location.hash
+    const actualPathname = isPathnameTheDefaultLanguage ? '/' : location.pathname
 
-let doAutomaticLanguageRedirection
-switch(automaticLanguageRedirectionStatus) {
-    case 'no':
-    case 'false':
-    case 'done':
+    const hashOfLocation = location.hash
+
+    let doAutomaticLanguageRedirection
+    switch(automaticLanguageRedirectionStatus) {
+        case 'no':
+        case 'false':
+        case 'done':
+            doAutomaticLanguageRedirection = false
+            break;
+        case 'yes':
+        case 'true':
+            doAutomaticLanguageRedirection = true
+            break;
+        default:
+            doAutomaticLanguageRedirection = (actualPathname === '/' && (! hashOfLocation)) ? true : false
+    }
+
+
+    // this is an assumption that browserLang (pt or pt-PT, for example) corresponds to the pathname (/pt/ or /pt-PT/, following the example)
+    const languageTargetPathname = (isBrowserLanguageSameLanguageAsRootWebpage) ? '/' : ( isPathnameTheDefaultLanguage ? '/' : `/${browserLang}/` )
+
+    // if you are not on a language page (or locale page); dont do redirection
+    if ( ! pathnamesOfAllLanguagesOfWebsite.find(str => str.startsWith(actualPathname)) ) {
         doAutomaticLanguageRedirection = false
-        break;
-    case 'yes':
-    case 'true':
-        doAutomaticLanguageRedirection = true
-        break;
-    default:
-        doAutomaticLanguageRedirection = (actualPathname === '/' && (! hashOfLocation)) ? true : false
-}
+    }
 
 
-// this is an assumption that browserLang (pt or pt-PT, for example) corresponds to the pathname (/pt/ or /pt-PT/, following the example)
-const languageTargetPathname = (isBrowserLanguageSameLanguageAsRootWebpage) ? '/' : ( isPathnameTheDefaultLanguage ? '/' : `/${browserLang}/` )
-
-// if you are not on a language page (or locale page); dont do redirection
-if ( ! pathnamesOfAllLanguagesOfWebsite.find(str => str.startsWith(actualPathname)) ) {
-    doAutomaticLanguageRedirection = false
-}
-
-
-// maybe do the redirection ??
-if (doAutomaticLanguageRedirection) {
-    urlParams.set('languageRedirection', "done")
-    const urlParamsString = urlParams.toString()
-    const targetHref = `${location.origin}${languageTargetPathname}?${urlParamsString}${hashOfLocation}`
-    location.replace(targetHref)
-}
+    // maybe do the redirection ??
+    if (doAutomaticLanguageRedirection) {
+        urlParams.set('languageRedirection', "done")
+        const urlParamsString = urlParams.toString()
+        const targetHref = `${location.origin}${languageTargetPathname}?${urlParamsString}${hashOfLocation}`
+        location.replace(targetHref)
+    }
+})();
